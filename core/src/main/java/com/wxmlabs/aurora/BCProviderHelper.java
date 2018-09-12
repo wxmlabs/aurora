8aacacd61cd7902bbb2cf6feb231263898d06097
Shining <shining@vshining.com>
2018-09-12 21:52:12 +0800

    复用JcaX509CertificateConverter实现BCProviderHelper.convertCertificate(org.bouncycastle.cert.X509CertificateHolder)

--- core/src/main/java/com/wxmlabs/aurora/BCProviderHelper.java
+++ core/src/main/java/com/wxmlabs/aurora/BCProviderHelper.java
@@ -1,31 +1,26 @@
 package com.wxmlabs.aurora;
 
 import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
 import org.bouncycastle.jcajce.util.BCJcaJceHelper;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.security.Provider;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 class BCProviderHelper extends BCJcaJceHelper {
     static final BCProviderHelper INSTANCE = new BCProviderHelper();
+    private static JcaX509CertificateConverter certificateConverter = new JcaX509CertificateConverter();
+
+    static {
+        certificateConverter.setProvider(INSTANCE.getProvider());
+    }
 
     Provider getProvider() {
         return provider;
     }
 
-    /*
-     * @see org.bouncycastle.operator.jcajce.OperatorHelper.convertCertificate
-     */
-    public X509Certificate convertCertificate(X509CertificateHolder certHolder) throws CertificateException {
-        try {
-            CertificateFactory certFact = INSTANCE.createCertificateFactory("X.509");
-            return (X509Certificate) certFact.generateCertificate(new ByteArrayInputStream(certHolder.getEncoded()));
-        } catch (IOException e) {
-            throw new CertificateException("cannot get encoded form of certificate: " + e.getMessage(), e);
-        }
+    X509Certificate convertCertificate(X509CertificateHolder certificateHolder) throws CertificateException {
+        return certificateConverter.getCertificate(certificateHolder);
     }
 }
